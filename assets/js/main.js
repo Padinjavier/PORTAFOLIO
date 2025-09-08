76e9bf17ed2583f2fef05b9bc92b9c8c5d436a2e
Padinjavier <javierpadin661@mail.com>
2025-09-08 15:03:51 -0500

    carga de CV

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,84 +18,33 @@ document.addEventListener("DOMContentLoaded", () => {
     })
 });
 
+// nav 
+const toggleBtn = document.getElementById("theme-toggle");
+const root = document.documentElement;
 
-// // Proyectos
-// document.addEventListener("DOMContentLoaded", () => {
-//     const username = "Padinjavier";
+const currentTheme = localStorage.getItem("theme") || "light";
+root.setAttribute("data-theme", currentTheme);
+toggleBtn.innerHTML = currentTheme === "light"
+    ? "<i class='bi bi-moon-fill'></i>"
+    : "<i class='bi bi-brightness-low-fill'></i>";
 
-//     // 🔹 Lista de repos que SÍ quieres mostrar
-//     const reposPermitidos = [
-//         "API-RICK-AND-MORTY",
-//         "MediPie",
-//         "vivetuaventura",
-//         "market.v2",
-//         "market",
-//         "Portal_MDQ"
-//     ];
+toggleBtn.addEventListener("click", () => {
+    const newTheme = root.getAttribute("data-theme") === "light" ? "dark" : "light";
+    root.setAttribute("data-theme", newTheme);
+    localStorage.setItem("theme", newTheme);
 
-//     // 🔹 Ruta de tu imagen fallback (sin rutas de C:)
-//     const fallbackImage = "./assets/images/proyectos/enproceso.png";
+    toggleBtn.innerHTML = newTheme === "light"
+        ? "<i class='bi bi-moon-fill'></i>"
+        : "<i class='bi bi-brightness-low-fill'></i>";
+});
 
-//     fetch(`https://api.github.com/users/${username}/repos?sort=updated`)
-//         .then(async res => {
-//             if (!res.ok) {
-//                 throw new Error(`HTTP error! status: ${res.status}`);
-//             }
-//             return res.json();
-//         })
-//         .then(async repos => {
-//             if (!Array.isArray(repos)) {
-//                 console.error("La API no devolvió un array:", repos);
-//                 return;
-//             }
+// Detecta tema SO al inicio
+if (!localStorage.getItem("theme")) {
+    const prefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+    root.setAttribute("data-theme", prefersDark ? "dark" : "light");
+}
 
-//             const container = document.getElementById("repos");
-//             const cols = 5;
 
-//             // 🔹 Filtrar solo los que estén en tu lista
-//             const reposFiltrados = repos.filter(repo =>
-//                 reposPermitidos.includes(repo.name)
-//             );
-
-//             const fillerNeeded = (cols - (reposFiltrados.length % cols)) % cols;
-
-//             for (let repo of reposFiltrados) {
-//                 const col = document.createElement("div");
-//                 col.className = "col-md-4 d-flex justify-content-center"; // 👈 3 columnas
-
-//                 const imageUrl = `https://raw.githubusercontent.com/${username}/${repo.name}/refs/heads/main/preview.png`;
-
-//                 col.innerHTML = `
-//                     <div class="repo-card p-2 text-center card-proyect elevar-card">
-//                      <div>      
-//                         <img src="${imageUrl}" alt="${repo.name}" 
-//                             onerror="this.onerror=null;this.src='${fallbackImage}'" 
-//                             style="width:100%;max-width:350px;height:200px;object-fit:cover;border-radius:12px;">
-//                         <h6 class="mt-2">${repo.name}</h6>
-//                         <p class="text-muted">${repo.description || "Sin descripción"}</p>
-//                     </div>
-//                     <div>
-//                         <a href="${repo.html_url}" target="_blank">
-//                             <i class="bi bi-github fs-3"></i>
-//                         </a>
-//                     </div>
-//                     </div>
-//                     `;
-
-//                 container.appendChild(col);
-//             }
-
-//             // 🔹 Rellenar espacios vacíos
-//             for (let i = 0; i < fillerNeeded; i++) {
-//                 const col = document.createElement("div");
-//                 col.className = "col d-flex justify-content-center";
-//                 col.innerHTML = `<div class="repo-card" style="opacity:0;"></div>`;
-//                 container.appendChild(col);
-//             }
-//         })
-//         .catch(err => console.error("Error al obtener repos:", err));
-// });
-// // End proyectos
 
 // Proyectos
 document.addEventListener("DOMContentLoaded", () => {
